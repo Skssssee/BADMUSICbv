9772ddd8f718e2aa87cff76323852f5a48c5f10c
sidhubad143 <lakhwinder4332@gmail.com>
2025-12-10 14:10:30 +0530

    Update mongo.py

--- BADMUSIC/core/mongo.py
+++ BADMUSIC/core/mongo.py
@@ -1,37 +1,18 @@
-# Copyright (C) 2024 by Badhacker98@Github, < https://github.com/Badhacker98 >.
-# Owner https://t.me/ll_BAD_MUNDA_ll
+from motor.motor_asyncio import AsyncIOMotorClient
 
-from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
-from pymongo import MongoClient
-from pyrogram import Client
-
-import config
+from config import MONGO_DB_URI
 
 from ..logging import LOGGER
 
-TEMP_MONGODB = "mongodb+srv://BADMUNDA:BADMYDAD@badhacker.i5nw9na.mongodb.net/"
+LOGGER(__name__).info("Connecting to your Mongo Database...")
+try:
+    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
+    mongodb = _mongo_async_.Alexa
+    LOGGER(__name__).info("Connected to your Mongo Database.")
+except Exception:
+    LOGGER(__name__).error("Failed to connect to your Mongo Database.")
+    exit()
 
-
-if config.MONGO_DB_URI is None:
-    LOGGER(__name__).warning(
-        "𝐍o 𝐌ONGO 𝐃B 𝐔RL 𝐅ound.. 𝐘our 𝐁ot 𝐖ill 𝐖ork 𝐎n 𝐁ᴀᴅ 𝐌𝐔𝐒𝐈𝐂 𝐃atabase"
-    )
-    temp_client = Client(
-        "BADMUSIC",
-        bot_token=config.BOT_TOKEN,
-        api_id=config.API_ID,
-        api_hash=config.API_HASH,
-    )
-    temp_client.start()
-    info = temp_client.get_me()
-    username = info.username
-    temp_client.stop()
-    _mongo_async_ = _mongo_client_(TEMP_MONGODB)
-    _mongo_sync_ = MongoClient(TEMP_MONGODB)
-    mongodb = _mongo_async_[username]
-    pymongodb = _mongo_sync_[username]
-else:
-    _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
-    _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
-    mongodb = _mongo_async_.BADMUSIC
-    pymongodb = _mongo_sync_.BADMUSIC
+## Database For Broadcast Subscription By Team Alexa
+MONGODB_CLI = AsyncIOMotorClient(MONGO_DB_URI)
+db = MONGODB_CLI["subscriptions"]
